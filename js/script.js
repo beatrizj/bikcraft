1e8955ddebb55010a626bb86ca5edcaf3ba0f03b
Beatriz Januário <bibi_januario@hotmail.com>
2025-04-16 15:28:26 -0300

    bikes gallery

--- js/script.js
+++ js/script.js
@@ -33,3 +33,17 @@ questions.forEach((question) => {
         question.setAttribute("aria-expanded", active)
     })
 })
+
+//Bikes gallery
+const images = document.querySelectorAll(".bike-img img")
+const imagesContainer = document.querySelector(".bike-img")
+
+images.forEach((image) => {
+    image.addEventListener("click", (e) => {
+        const media1000px = matchMedia("(min-width: 920px)").matches
+        const media800px = matchMedia("(max-width: 800px)").matches
+        if (media1000px || media800px) {
+            imagesContainer.prepend(e.currentTarget)
+        }        
+    })
+})
