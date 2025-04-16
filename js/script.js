d04c10f97d88b35b3c6d8f9c3637738ec7af95a9
Beatriz Januário <bibi_januario@hotmail.com>
2025-04-16 12:33:20 -0300

    budget parameters

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+//Links do menu
 const links = document.querySelectorAll(".header-menu a")
 
 links.forEach((link) => {
@@ -5,5 +6,15 @@ links.forEach((link) => {
 
     if(window.location.href.includes(href)) {
         link.classList.add("ativo")
+    }
+})
+
+//Itens do orçamento
+const parameters = new URLSearchParams(location.search)
+
+parameters.forEach((parameter) => {
+    const element = document.getElementById(parameter)
+    if (element) {
+        element.checked = true
     }
 })
